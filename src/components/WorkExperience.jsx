74d5164ad55bff50e288214ceeaf59ba15dbebc7
Ismail El Azhari <95006185+Ismail-El-Azhari@users.noreply.github.com>
2025-02-22 14:42:30 -0500

    fixed WorkExperience links

--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 function WorkExperience(){
+  const handleButtonClick = (url) => {
+    console.log("Button clicked! Opening:", url);
+    window.open(url, '_blank');
+  };
+
   return(
     <div class="section">
     <h2>Work Experience:</h2>
@@ -12,11 +17,11 @@ function WorkExperience(){
       <p>•  Employed Microsoft Excel to process, clean and transform data, and analyze it to better inform business decisions.</p>
       <p>•	Created interactive and insightful data visualizations using Power BI.</p>
       <p class="dates">May 2023- Aug 2023</p>
-      <div>
-        <button class="website-button" data-link="https://www.brp.com">BRP website</button>
+      <div> 
+        <button class="website-button" onClick ={() => handleButtonClick("https://www.brp.com")}>BRP website</button>
       </div>
       <div>
-        <button class="video-button" data-link="https://youtu.be/KwKCG26S5WM">Video representation (in French)</button>
+        <button class="video-button" onClick={() => handleButtonClick("https://youtu.be/KwKCG26S5WM")}>Video representation (in French)</button>
       </div>
     </div>
     <div class="section-example">
@@ -28,7 +33,7 @@ function WorkExperience(){
       <p>•	Provided technical support and troubleshooting assistance to customers.</p>
       <p>•	Handled and resolved billing inquiries and concerns.</p>
       <p class="dates">Apr 2019 - Sep 2022</p>
-      <button class="website-button" data-link="https://www.rogers.com">Rogers website</button>
+      <button class="website-button" onClick={() => handleButtonClick("https://www.rogers.com")}>Rogers website</button>
     </div>
     <div class="section-example">
       <h3>Customer  service  representative</h3>
@@ -38,7 +43,7 @@ function WorkExperience(){
         <p>•	Provided technical support and troubleshooting assistance to customers.</p>
         <p>•	Engaged with customers on social media platforms, delivering the same level of support and assistance as provided during inbound calls.</p>
         <p class="dates">Feb 2018 - Mar 2019</p>
-        <button class="website-button" data-link="https://www.voxdata.com">Voxdata website</button>
+        <button class="website-button" onClick={() => handleButtonClick("https://www.voxdata.com")}>Voxdata website</button>
     </div>
   </div>
 
